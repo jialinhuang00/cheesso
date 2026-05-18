143c831c1dd26df0c9e1ecdf8ace85fa8c241f5f
jialin <linao264590000@gmail.com>
2026-05-18 15:01:07 +0800

    chore(release): v1.1.4

--- src/version.ts
+++ src/version.ts
@@ -1,5 +1,5 @@
 // This will be replaced during build with actual commit hash
-const BUILD_COMMIT = '279034d';
+const BUILD_COMMIT = '4a311e3';
 
 export function getVersion(): string {
   // Check if running in Node.js environment
